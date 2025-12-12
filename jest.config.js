a3549062cd1fc0137e615143a186345dc23b835f
Marcos Sanchez <marsanmar6291@gmail.com>
2025-12-12 13:22:12 -0600

    chore(test): update Jest config to support TS ignoreDeprecations 6.0 and fix path mapping

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
   },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1", 
-  },
+    "^@/(.*)$": "<rootDir>/$1"
+  }
 };
